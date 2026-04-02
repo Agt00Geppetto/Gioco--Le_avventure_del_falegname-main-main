c9eca2134f9f2c43078b8a124a7b5b914b149651
Agt00Geppetto <gabriele.bonaventura@itiszuccante.edu.it>
2026-04-02 09:40:39 +0200

    lasciamo stare le piattaforme elaborate, il nome che gli ho dato è giustissimo

--- piattaforme.py
+++ piattaforme.py
@@ -11,9 +11,17 @@ class Piattaforme:
     def crea_piattaforme(self):
 
         # Piattafroma base
-        for x in range (200, 1000, 150):
+        for x in range (200, 2800, 300):
             piattaforma = arcade.Sprite("./assets/piattaforma-new.png")
             piattaforma.center_x = x
             piattaforma.center_y = 180
             piattaforma.scale = 0.1
             self.scene.add_sprite("Piattaforme", piattaforma)
+
+        # Piattaforme elaborate
+        # for x in range (1001, 2000):
+        # piattaforma_e = arcade.Sprite("./assets/piattaforma-elaborata.png")
+        # piattaforma_e.center_x = 300
+        # piattaforma_e.center_y = 200
+        # piattaforma_e.scale = 0.5
+        # self.scene.add_sprite("Piattaforme", piattaforma_e)
